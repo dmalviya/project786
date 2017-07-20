772bf91a5e77f7fac27428529395bbc6e548b64c
dmalviya <30290365+dmalviya@users.noreply.github.com>
2017-07-20 11:32:25 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,9 +2,7 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		System.out.println(" Hello i am From Git-HUB ");
-		System.out.println(" Hello i am From Git-HUB ");
-		System.out.println(" Hello i am From Git-HUB ");
-		System.out.println(" Hello i am From Git-HUB ");	
+		System.out.println(" Hello i am Modified Code !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ");
+			
 	}
 }
